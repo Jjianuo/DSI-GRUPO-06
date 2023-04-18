857fca4797658f7e21a1ba32995f3ba437308d45
maria <marsache@ucm.es>
2023-04-18 10:48:23 +0200

    loadingscreen terminada

--- RADIANT SPARK/LoadingScreen.xaml.cs
+++ RADIANT SPARK/LoadingScreen.xaml.cs
@@ -25,6 +25,43 @@ namespace RADIANT_SPARK
         public LoadingScreen()
         {
             this.InitializeComponent();
+            DispatcherTimerSetup();
+        }
+
+        DispatcherTimer dispatcherTimer;
+        DateTimeOffset startTime;
+        DateTimeOffset lastTime;
+        DateTimeOffset stopTime;
+        int timesTicked = 1;
+        int timesToTick = 1;
+
+        public void DispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            startTime = DateTimeOffset.Now;
+            lastTime = startTime;
+            dispatcherTimer.Start();
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            DateTimeOffset time = DateTimeOffset.Now;
+            TimeSpan span = time - lastTime;
+            lastTime = time;
+            timesTicked++;
+            if (timesTicked > timesToTick)
+            {
+                myProgressBar.Value += 4;
+                stopTime = time;
+                span = stopTime - startTime;
+            }
+            if (myProgressBar.Value >= 100)
+            {
+                Frame.Navigate(typeof(InGame));
+                dispatcherTimer.Stop();
+            }
         }
     }
 }
